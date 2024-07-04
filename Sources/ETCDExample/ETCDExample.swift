00f565fedab422be0c6e9af10373d1785d2a204f
“Ayushi <attiwari@uwaterloo.ca>
2024-07-03 20:55:07 -0400

    re-add header

--- Sources/ETCDExample/ETCDExample.swift
+++ Sources/ETCDExample/ETCDExample.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-etcd-client-gsoc open source project
+//
+// Copyright (c) 2024 Apple Inc. and the swift-etcd-client-gsoc project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of swift-etcd-client-gsoc project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 import ETCD
 import NIO
 import Dispatch
